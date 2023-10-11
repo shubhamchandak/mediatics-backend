418cf364fb3a449694cf5d5b7d274a9ff880fb94
shubhamchandak <shubhamchandak8@gmail.com>
2023-10-10 21:30:32 -0400

    format error response

--- src/utils/routerUtils.ts
+++ src/utils/routerUtils.ts
@@ -3,7 +3,7 @@ import { ServiceResponse } from "../models/serviceResponse.js";
 
 export function sendResponse(response: ServiceResponse, res: Response) {
     if(response.error) {
-      return res.status(response.error.errorCode).json({message: response.error});
+      return res.status(response.error.errorCode).json(response.error);
     }
     res.status(response.statusCode || 200).json({data: response.data});
   }
